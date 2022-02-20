e8d4a59d0a4f0f9297e83bf943f45974a7f84dfc
bigrockquiz <robert.sundin1996@gmail.com>
2022-02-20 12:36:13 +0100

    tasks added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 function App() {
   const name = 'Brad';
   const x = false;
 
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Task 1',
+      day: 'Feb 5',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Task 2',
+      day: 'Feb 6',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Task 3',
+      day: 'Feb 7',
+      reminder: false,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header title={1} />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
